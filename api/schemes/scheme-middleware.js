7e756000a146a24d77b59d3749dbe479a17568c4
MMostella <masonmostella@gmail.com>
2021-10-06 17:59:20 -0500

    Fleshin out bugs. 10 tests still failing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -9,21 +9,25 @@ const Scheme = require("./scheme-model");
   }
 */
 const checkSchemeId = async (req, res, next) => {
-  // try {
-  //   const scheme = await Scheme.findById(req.params.id);
-  //   const schemeSteps = await Scheme.findSteps(req.params.id);
-  //   if (scheme) {
-  //     next();
-  //   } else {
-  //     next({
-  //       status: 404,
-  //       message: `scheme with scheme_id ${req.params.id} not found`,
-  //     });
-  //   }
-  // } catch (err) {
-  //   next(err);
-  // }
-  next();
+  try {
+    const scheme = await Scheme.findById(req.params.scheme_id);
+    const schemeSteps = await Scheme.findSteps(req.params.scheme_id);
+    if (scheme) {
+      if (schemeSteps) {
+        next();
+      } else {
+        res.status(201).json([]);
+      }
+    } else {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+  // next();
 };
 
 /*
@@ -35,6 +39,16 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  // const { scheme_name } = req.body;
+  // if (
+  //   scheme_name === undefined ||
+  //   scheme_name === " " ||
+  //   typeof scheme_name != "string"
+  // ) {
+  //   next({ status: 400, message: "invalid scheme_name" });
+  // } else {
+  //   next();
+  // }
   next();
 };
 
@@ -48,6 +62,18 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  // const { step_number, instructions } = req.body;
+  // if (
+  //   instructions === undefined ||
+  //   instructions === " " ||
+  //   typeof instructions != "string"
+  // ) {
+  //   next({ status: 400, message: "invalid step" });
+  // } else if (step_number < 1 || typeof step_number != "number") {
+  //   next({ status: 400, message: "invalid step" });
+  // } else {
+  //   next();
+  // }
   next();
 };
 
--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -5,7 +5,8 @@ function find() {
     .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
     .select("sc.scheme_id", "sc.scheme_name")
     .count("st.step_id as number_of_steps")
-    .groupBy("sc.scheme_id");
+    .groupBy("sc.scheme_id")
+    .orderBy("sc.scheme_id", "asc");
 }
 
 async function findById(scheme_id) {
@@ -18,15 +19,17 @@ async function findById(scheme_id) {
       "st.instructions",
       "sc.scheme_id"
     )
-    .where("sc.scheme_id", scheme_id);
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
+  // console.log(data);
 
   const result = {
     scheme_id: data[0].scheme_id,
     scheme_name: data[0].scheme_name,
     steps:
-      data[0].length === 0
+      data.step_id === undefined
         ? []
-        : data.map((scheme) => {
+        : data.forEach((scheme) => {
             return {
               step_id: scheme.step_id,
               step_number: scheme.step_number,
@@ -41,7 +44,8 @@ function findSteps(scheme_id) {
   return db("steps as st")
     .join("schemes as sc", "st.scheme_id", "sc.scheme_id")
     .select("st.step_id", "st.step_number", "st.instructions", "sc.scheme_name")
-    .where("sc.scheme_id", scheme_id);
+    .where("sc.scheme_id", scheme_id)
+    .orderBy("st.step_number", "asc");
 }
 
 async function add(scheme) {
@@ -49,14 +53,14 @@ async function add(scheme) {
   return findById(id);
 }
 
-function addStep(scheme_id, step) {
+async function addStep(scheme_id, step) {
   // EXERCISE E
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
-  return db("steps").insert(step).where("scheme_id", scheme_id);
+  return db("steps").insert(step).into("steps").where("scheme_id", scheme_id);
 }
 
 module.exports = {
